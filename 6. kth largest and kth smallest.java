99fbe5734d89c0314431e21fd6a7438e1a20153a
Arkaprava Ghosh <62136090+ribhu69@users.noreply.github.com>
2021-05-11 11:52:47 +0530

    Create 6. kth largest and kth smallest.java

---
+++ 6. kth largest and kth smallest.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class basics
+{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many Numbers");
+        int n = sc.nextInt();
+        Set<Integer> set1= new TreeSet<>();
+        System.out.println("enter the numbers");
+        for(int i=0;i<n;i++)
+        {
+            set1.add(sc.nextInt());
+        }
+        System.out.println("Enter kth value");
+        n=set1.size();
+        int k=sc.nextInt();
+        if(k>n)
+        {
+            System.out.println("Invalid Input");
+        }
+        List<Integer> list1 = new ArrayList<>(set1);
+        System.out.println("largest element is "+ list1.get(k-1));
+        System.out.println("smallest element is "+ list1.get(n-k));
+    }
+}
