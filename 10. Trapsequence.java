c3b2a8296c15e76cdd3ac47d715d796e7453e02b
Arkaprava Ghosh <62136090+ribhu69@users.noreply.github.com>
2021-05-12 14:34:55 +0530

    Update 10. Trapsequence.java

--- 10. Trapsequence.java
+++ 10. Trapsequence.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class basics {
     public static List<Integer> trapSeq = new ArrayList<>();
+    static int x,y;
 
     static boolean multiple(int x, int y)
     {
@@ -34,33 +35,40 @@ class basics {
         return sum;
     }
 
+    static boolean questionCondition(int x,int y) {
+        if (x == y || multiple(x, y))
+            return true;
+        else
+            while (String.valueOf(x).length() != 1) {
+                x = digitSum(x);
+                if (multiple(x, y))
+                    return true;
+            }
+
+            return false;
+    }
+
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        int n1,n2,y;
+        Scanner sc = new Scanner(System.in);
+        int n1, n2;
         n1=sc.nextInt();
         n2=sc.nextInt();
         y=sc.nextInt();
-
         for(int i=n1;i<=n2;i++)
         {
-            int x=i;
-            while(String.valueOf(x).length()!=1)
-                x = digitSum(x);
-
+            x=i;
             if(y<3)
-            {
                 y=3;
-            }
-            if(x>0 && ((x==y) || multiple(x,y)))
+
+            if(questionCondition(x,y))
             {
                 trapSeq.add(i);
                 y+=2;
             }
             else
-            {
                 y-=1;
-            }
+
         }
         if(trapSeq.size()>0)
             System.out.println(trapSeq);
