d217e704ffd9b15c7fd704370b0b7ca954f2f220
Arkaprava Ghosh <62136090+ribhu69@users.noreply.github.com>
2021-05-13 16:49:41 +0530

    Create interestCharge.java

---
+++ atm/interestCharge.java
@@ -0,0 +1,10 @@
+public class interestCharge {
+
+    public double interestCharge(double amount)
+    {
+        if(amount<=100)
+            return 0.02;
+        else
+            return 0.04;
+    }
+}
