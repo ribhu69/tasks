fa10cc376d6f28212358c7e4e3db836ddb7aae2c
Arkaprava Ghosh <62136090+ribhu69@users.noreply.github.com>
2021-05-13 11:19:08 +0530

    Update 7. reverse_a_string.java

--- 7. reverse_a_string.java
+++ 7. reverse_a_string.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 class basics {
 
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string");
-        StringBuffer stringValue = new StringBuffer();
-        stringValue.append(sc.nextLine());
-        stringValue.reverse();
-        System.out.println("Reversed String: " + stringValue.toString());
+        String value = sc.nextLine();
+        System.out.print("Reversed String is: ");
+        for(int i = value.length()-1;i>=0;i--)
+            System.out.print(value.charAt(i));
     }
 }
