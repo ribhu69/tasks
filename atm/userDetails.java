d5676861b108fec377cf64265718016db9df1850
Arkaprava Ghosh <62136090+ribhu69@users.noreply.github.com>
2021-05-14 08:59:00 +0530

    Update userDetails.java

--- atm/userDetails.java
+++ atm/userDetails.java
@@ -4,16 +4,42 @@ public class userDetails {
     private long userAccountID;
     private double userAccountBalance;
     private String cardNumber;
+    private int pin;
 
-    public void userDetail(String userAccountName, long userAccountId, double userAccountBalance, String cardNumber) {
+
+    public userDetails(String userAccountName, long userAccountId, double userAccountBalance, String cardNumber, int pin) {
         this.userAccountName = userAccountName;
         this.userAccountID = userAccountId;
         this.userAccountBalance = userAccountBalance;
         this.cardNumber = cardNumber;
+        this.pin=pin;
+
     }
 
     public void getAccountBalance() {
         System.out.println("Account Balance : " + this.userAccountBalance);
+    }
+
+
+
+    public String getCardNumber()
+    {
+        return this.cardNumber;
+    }
+
+    public String getUserAccountName()
+    {
+        return this.userAccountName;
+    }
+
+    public long getUserAccountID()
+    {
+        return this.userAccountID;
+    }
+
+    public int getPin()
+    {
+        return this.pin;
     }
 
     public double savingAccountBalance() {
@@ -24,6 +50,10 @@ public class userDetails {
         swipeTransaction(cashback);
     }
 
+    private void swipeTransaction(double cashback) {
+        System.out.println("Cashback :" + cashback + "\nBalance USD: " + this.userAccountBalance);
+    }
+
     public void withDrawTransactionInfo(int withdrawAmt, double charges) {
         withDrawTransaction(withdrawAmt, charges);
     }
@@ -32,12 +62,14 @@ public class userDetails {
         System.out.println("Withdrawn Amount: " + withdrawAmt + "\nCharges :" + charges + "\nBalance USD: " + this.userAccountBalance);
     }
 
-    private void swipeTransaction(double cashback) {
-        System.out.println("Cashback :" + cashback + "\nBalance USD: " + this.userAccountBalance);
-    }
-
     public void updateAccountBalance(double updatedBalance) {
         this.userAccountBalance = updatedBalance;
         System.out.println("Account Balance :" + this.userAccountBalance);
     }
+
+    public void setUserAccountBalance(double newBalance)
+    {
+        this.userAccountBalance= newBalance;
+    }
+
 }
