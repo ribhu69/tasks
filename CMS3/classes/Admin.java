92c4835de5a38f9d876a9b66fb45c74151fd2738
Arkaprava Ghosh <62136090+ribhu69@users.noreply.github.com>
2021-06-22 17:55:54 +0530

    Add files via upload

---
+++ CMS3/classes/Admin.java
@@ -0,0 +1,39 @@
+package classes;
+
+public class Admin {
+
+    private String adminName;
+    private String adminID;
+    private String password;
+
+
+    public Admin(String adminName, String adminID, String password)
+    {
+        this.adminID=adminID;
+        this.adminName=adminName;
+        this.password = password;
+    }
+
+    public String getAdminID()
+    {
+        return this.adminID;
+    }
+
+    public String getAdminName() {return this.adminName;}
+
+    public String getAdminPin()
+    {
+        return this.password;
+    }
+
+    public void setAdminID(String adminID) {
+        this.adminID = adminID;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ CMS3/classes/Course.java
@@ -0,0 +1,77 @@
+package classes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Course {
+    private String courseName;
+    private String courseTeacher;
+    private final int courseYear;
+    private final int courseSemester;
+    private int courseTeacherID;
+    private final String courseID;
+    private final List<String> courseTopics = new ArrayList<>();
+
+
+    Scanner sc = new Scanner(System.in);
+
+
+    public void iterateCourseTopics()
+    {
+
+        for(int i=0;i<courseTopics.size();i++)
+        {
+            System.out.println((i+1) + ") "+courseTopics.get(i));
+        }
+    }
+
+    public int getCourseYear()
+    {return this.courseYear;}
+
+    public Course(String courseName,String courseId,String courseTeacher,int courseTeacherID, int courseYear,int courseSemester)
+    {
+        this.courseName=courseName;
+        this.courseID = courseId;
+        this.courseTeacher=courseTeacher;
+        this.courseTeacherID=courseTeacherID;
+        this.courseYear=courseYear;
+        this.courseSemester=courseSemester;
+    }
+
+    public void modifyCourseTeacher(String courseTeacher)
+    {
+        this.courseTeacher=courseTeacher;
+    }
+
+
+    public void setCourseTeacherID(int teacherID) {this.courseTeacherID=teacherID;}
+    public String getCourseName()
+    {
+        return this.courseName;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public String getCourseTeacher()
+    {
+        return this.courseTeacher;
+    }
+
+    public void setCourseName(String courseName) { this.courseName=courseName; }
+
+    public int getCourseSemester() {
+        return courseSemester;
+    }
+
+    public int getCourseTeacherID() {
+        return courseTeacherID;
+    }
+
+    public void setCourseTeacher(String courseTeacher) {
+        this.courseTeacher = courseTeacher;
+    }
+}
+
